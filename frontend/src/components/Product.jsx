6cfc937d84e853a3908dd9853435ff7bf924b009
= <=>
2026-07-30 15:57:55 -0400

    Rating Component

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,5 +1,6 @@
 import {Card, card} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
+import Rating from './Rating'
 
 const Product = ({product}) => {
   return (
@@ -10,10 +11,14 @@ const Product = ({product}) => {
 
       <Card.Body>
         <Link to={`/product/${product._id}`}>
-          <Card.Title as="div">
+          <Card.Title as="div" className='product-title'>
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
+
+        <Card.Text as="div">
+          <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'}/>
+        </Card.Text>
 
         <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
