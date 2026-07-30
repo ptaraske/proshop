ef4129bd91c4796b07e0f2a017c601f8e867c065
= <=>
2026-07-30 18:44:44 -0400

    Axios fetch products

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,12 +1,23 @@
-import {useParams} from 'react-router-dom'
-import {Link} from 'react-router-dom'
+import {useState, useEffect} from 'react'
+import {useParams, Link} from 'react-router-dom'
 import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
 const ProductScreen = () => {
+    const [product, setProduct] = useState({});
     const {id: productID} = useParams();
-    const product = products.find((p) => p._id === productID);
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+        const {data} = await axios.get(`/api/products/${productID}`);
+        setProduct(data);
+    };
+
+    fetchProduct();
+  }, [productID]);
+
+
 
     return (
         <>
